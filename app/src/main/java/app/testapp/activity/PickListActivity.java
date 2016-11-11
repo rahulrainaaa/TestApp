b47c1a645cadb145751578e46c96b5ac23f26033
rahulrainahome <rahulrainahome@live.com>
2016-11-12 02:47:50 +0530

    Initial Commit

---
+++ app/src/main/java/app/testapp/activity/PickListActivity.java
@@ -0,0 +1,15 @@
+package app.testapp.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import app.testapp.R;
+
+public class PickListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_list);
+    }
+}
