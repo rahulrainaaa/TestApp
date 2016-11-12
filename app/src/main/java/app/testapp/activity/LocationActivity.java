e0d6fb7aeffeab8ff612298388d25f78edb27ad0
rahulrainahome <rahulrainahome@live.com>
2016-11-13 04:20:40 +0530

    Initial Commit

--- app/src/main/java/app/testapp/activity/LocationActivity.java
+++ app/src/main/java/app/testapp/activity/LocationActivity.java
@@ -1,8 +1,7 @@
 package app.testapp.activity;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import app.testapp.R;
 
